9a057d53e740bf69095696e6721d6945ce4be217
bishalrajbanshi <bishalrajbanshi.mail@gmail.com>
2025-09-04 16:12:25 +0545

    Refactor code execution logic into CodeServices and remove deprecated file handling

--- src/modules/dev/controller/dev_controller.ts
+++ src/modules/dev/controller/dev_controller.ts
@@ -1,38 +1,11 @@
 import { Request, Response } from "express";
-import { exec } from "child_process";
-import fs from "fs-extra";
-import { generateFileName } from "../utils/generate_filename";
-import { commands, dockerImages, fileExt } from "../utils/docker_image_command_file";
+import { codeServices } from "../services/dev_services";
 
 export const runCode = async (req: Request, res: Response) => {
   const { code, language } = req.body;
-
-  if (!dockerImages[language]) {
-    return res.status(400).json({ error: "Unsupported language" });
-  }
-  const fileId = generateFileName();
-  const tempDir = "submissions";
-  await fs.ensureDir(tempDir);
-
-  const fileName = `${tempDir}/${fileId}.${fileExt[language]}`;
-  console.log(fileName);
-  
-
   try {
-    await fs.outputFile(fileName, code);
-    const hostPath = process.cwd().replace(/\\/g, "/");
-    const dockerCmd = `docker run --rm -v "${hostPath}:/usr/src/app" -w /usr/src/app ${
-      dockerImages[language]
-    } bash -c "${commands[language](fileName)}"`;
-
-    exec(dockerCmd, async (err, stdout, stderr) => {
-      //   await fs.remove(fileName);
-      if (err) {
-        return res.json({ success: false, error: stderr || err.message });
-      }
-
-      res.json({ success: true, output: stdout.trim() });
-    });
+    const codeSnapit = await codeServices.SolveCode(code, language);
+    res.json({ success: true, result: codeSnapit });
   } catch (err: any) {
     res.json({ success: false, error: err.message });
   }
